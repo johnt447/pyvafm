79b3153e30d3fb0b2713cf4413c49cb0a3ac35e1
johnt447 <johntracey@wireless-86-50-135-137.open.aalto.fi>
2013-09-25 15:18:48 +0300

    started delay circuit

--- vafmcircuits_signal_processing.py
+++ vafmcircuits_signal_processing.py
@@ -186,3 +186,23 @@ class integral(Circuit):
 			self.yo = self.I["in"].value
 
 
+class Delay(Circuit):
+    
+    
+	def __init__(self, machine, name, **keys):
+		
+		super(self.__class__, self).__init__( machine, name )
+		
+		self.AddInput("in")
+
+	def Initialize (self):
+		
+		pass
+	
+		
+		
+	
+	def Update (self):
+
+
+
