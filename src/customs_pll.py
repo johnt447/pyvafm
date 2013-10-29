d107f8bf05cb047b8200850cfa338d5608fe4811
fullmetalfelix <vampirer@tiscalinet.it>
2013-10-29 17:23:13 +0900

    typos

--- src/customs_pll.py
+++ src/customs_pll.py
@@ -73,7 +73,7 @@ def aPLL(compo,**keys):
 		compo.AddCircuit(type='ActiveLowPass',name='lp'+str(i+1),fcut=f, pushed=True)
 	    
   	compo.AddCircuit(type='PI',name='pump', Kp=keys['Kp'],Ki=keys['Ki'], set=0, pushed=True)
-	compo.AddCircuit(type='Gain',name='dfgain', gain=keys['gain'], pushed=True)
+	compo.AddCircuit(type='gain',name='dfgain', gain=keys['gain'], pushed=True)
   	compo.AddCircuit(type='opAdd',name='fsum', pushed=True)
   	compo.AddCircuit(type='waver',name='vco', amp=1, pushed=True)
   	
@@ -83,12 +83,12 @@ def aPLL(compo,**keys):
   	compo.Connect("global.signal1","pfd.in1")
   	compo.Connect("global.signal2","pfd.in2")
   	
-  	compo.Connect("pfd.out","lp1.in")
+  	compo.Connect("pfd.out","lp1.signal")
 	for i in range(1,len(filters)):
-		compo.Connect("lp"+str(i)+".out","lp"+str(i+1)+".in")
+		compo.Connect("lp"+str(i)+".out","lp"+str(i+1)+".signal")
   	
   	compo.Connect("lp"+str(len(filters))+".out","pump.signal")
-  	compo.Connect("pump.out","dfgain.in")
+  	compo.Connect("pump.out","dfgain.signal")
   	compo.Connect("dfgain.out","fsum.in1","global.df")
   	compo.Connect("global.f0", "fsum.in2")
 	compo.Connect("fsum.out",   "vco.freq")
