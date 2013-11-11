5641f04d6c91066c5bd7c3ccd9f4e99148280c34
fullmetalfelix <vampirer@tiscalinet.it>
2013-11-11 14:44:57 +0900

    interpolation and scanner

--- src/vafmcircuits_Interpolation.py
+++ src/vafmcircuits_Interpolation.py
@@ -1,46 +1,43 @@
-import numpy 
+# -*- coding:utf-8 -*-
+## \package vafmcircuits_Interpolation
+# This file contains the interpolation for the force field.
+# \file vafmcircuits_Interpolation.py
+# This file contains the interpolation for the force field.
+
+import numpy
 import math
 from vafmbase import Circuit
 import math
 import vafmcircuits_Logic
 from vafmcircuits import Machine
 from scipy.interpolate import LinearNDInterpolator
-
-## \package vafmcircuits_Interpolation
-# This file contains the interpolation for the force field.
-#
-
+import os
+import re
 
 ## \brief Interpolation circuit.
 #
-# This is the circuit that calculates the interpolation of a C-dimensional field in a D-dimensional space.
-# The field must be in the following format: 
-# <pre> x y z Fx Fy Fz
-#         or         
-# x y z F
-#         or
-# x Fx
-#         or
-# x y Fx Fy </pre>
+# This is the circuit that caclaultes the interpolation of the force field provided. The force field must be in the following format, but plese note that the interpolation circuit is capable of taking
+# any number of dimensions and components in, but for a case where there is an unequal amount of dimensions and components the unused components column must be filled with zeros. Except in the case
+# of 3 dimensions and 1 component, this can be left as it is. Examples of how the force fields must be formated is shown below.: \n
+# <pre> x y z Fx Fy Fz or x y z F or x y z Fx 0 0 or x y Fx 0 <pre>
 #
-# \b Initialisation \b parameters:
-# 	- \a Filename  = Filename of the force field input file.
-#	- \a Dimensions = Number of dimensons in the force field.
-#	- \a Components	= Number of components of force in the force field.
+# - \b Initialisation \b parameters:
+# 	- \a Filename = Filename of the force field input file.
+# 	- \a Dimensions = Number of dimensons in the force field.
+# 	- \a Components = Number of components of force in the force field.
 #
-# \b Input \b channels: 
-# 	- \a coord : this is the coordiante to calcualte the interpolation.
+# - \b Input \b channels:
+#         - \a coord : this is the coordiante to calcualte the interpolation.
 #
-# \b Output \b channels: 
-#	- \a Fx: The interpolated forces x component. 
-#	- \a Fy: The interpolated forces y component. 
-#	- \a Fz: The interpolated forces z component. 
+# - \b Output \b channels:
+#        - \a Fx: The interpolated forces where x is the component for example F1 would be first first component.
 #
 # \b Example:
 # \code
-# field = machine.AddCircuit(type='Interpolate',name='inter', Filename='forces.dat', Dimensons=3, Components=3 , pushed=True)
+# inter = machine.AddCircuit(type='Interpolate',name='inter', Filename = 'Force.dat', Dimensions = 3, Components = 3 ,pushed=True)
 # \endcode
 #
+
 class Interpolate(Circuit):
     
     
@@ -48,15 +45,11 @@ class Interpolate(Circuit):
 		
 		super(self.__class__, self).__init__( machine, name )
 
-		self.AddInput("coord")
-		self.AddOutput("Fx")
-		self.AddOutput("Fy")
-		self.AddOutput("Fz")
-
 		self.minboundarycheck = False
 		self.maxboundarycheck = False
-
+		self.check = False
 		self.Components = 1
+		self.errorcheck = False
 
 		#print self.AddInput("Filename")
 
@@ -76,72 +69,46 @@ class Interpolate(Circuit):
 			raise NameError("Missing Components input!")
 
 
-		self.xvalues = []
-		self.Fx = []
+		f = open(self.Filename, "r")
+		for i in range(0,self.Components):
+			self.AddOutput("F" + str(i+1) )
 
-		self.yvalues = []
-		self.Fy = []
+		self.AddInput("coord")
 
-		self.zvalues = []
-		self.Fz = []
+		#making a number of arrrays equal to the number of Dimensons
+		self.pos = [[] for _ in range( self.Dimensions )]
+		#making a number of arrrays equal to the number of Components
+		self.comp = [[] for _ in range( self.Components )]
 
+		#Check the input and read it!
+		for line in f:
+			
+			#this is faster
+			counter = len(line.split())
+			
+			if counter != self.Dimensions + self.Components:
+				raise SyntaxError("ERROR: Incorrect number of Dimensions or Components entered!")
 
-		f= open (self.Filename)
+			#Fill Up the arrays
+			
+			words = line.split()
+			for i in range(0,self.Dimensions):
+				self.pos[i].append ( float(words[i]) )
 
-		if self.Dimensions == 1:
-			for line in f:
-				self.xvalues.append ( line.split()[0] )
-				self.Fx.append ( line.split()[self.Dimensions] )
-				self.Fy.append ( 0 )
-				self.Fz.append ( 0 )
+			for i in range(0 ,self.Components):
+				self.comp[i].append ( float(words[i + self.Dimensions]) )
 
-
-
-
-		if self.Dimensions == 2:
-			for line in f:
-				self.xvalues.append ( line.split()[0] )
-				self.Fx.append ( line.split()[self.Dimensions] )
-
-				self.yvalues.append ( line.split()[1] )
-				self.Fy.append ( line.split()[self.Dimensions + self.Components - 1] )
-
-				self.Fz.append ( 0 )
-
-
-		if self.Dimensions == 3 and self.Components == 3:
-			for line in f:
-				self.xvalues.append ( line.split()[0] )
-				self.Fx.append ( line.split()[self.Dimensions ] )
-
-				self.yvalues.append ( line.split()[1] )
-  				self.Fy.append ( line.split()[self.Dimensions + self.Components - 2] )
-
-				self.zvalues.append ( line.split()[2] )
-				self.Fz.append ( line.split()[self.Dimensions + self.Components - 1] )
-
+		# I dont get this?!
 		if self.Dimensions == 3 and self.Components == 1:
-			for line in f:
-				self.xvalues.append ( line.split()[0] )
-				self.yvalues.append ( line.split()[1] )
-				self.zvalues.append ( line.split()[2] )
-				self.Fx.append ( line.split()[self.Dimensions ] )
-			self.i = LinearNDInterpolator( (self.xvalues, self.yvalues, self.zvalues ), self.Fx  )
-
-		self.SetInputs(**keys)
-
-
-
-
-		
+			self.check = True
 
 	def Initialize (self):
 		
-		pass 
-		
+		pass
+	
 	## Setting the maximum x y and z value of the force field
 	#
-	# @param *args will take in a coordiante  and if the cantilever goes outwith this boundary a warning is printed on screen.
+	# @param *args will take in a coordiante and if the cantilever goes outwith this boundary a warning is printed on screen.
 	#
 	# \b Example:
 	# \code{.py}
@@ -149,7 +116,7 @@ class Interpolate(Circuit):
 	# inter = machine.AddCircuit(type='Interpolate',name='inter', Filename = 'Force.dat', Dimensons = 3, Components = 3 ,pushed=True)
 	# inter.Maxboundary(10,10,10)
 	# \endcode
-	#		
+	# 
 	# \code{.py}
 	# #For Dimensons = 2
 	# inter = machine.AddCircuit(type='Interpolate',name='inter', Filename = 'Force.dat', Dimensons = 2, Components = 2 ,pushed=True)
@@ -161,20 +128,22 @@ class Interpolate(Circuit):
 	# inter = machine.AddCircuit(type='Interpolate',name='inter', Filename = 'Force.dat', Dimensons = 1, Components = 1 ,pushed=True)
 	# inter.Maxboundary(10)
 	# \endcode
-
+	#
 	def Maxboundary(self, *args):
-		self.maxx = args[0]
+		self.max = []
 
-		if len(args) >= 2:
-			self.maxy = args[1]
-		if len(args) == 3:
-			self.maxz = args[2]
+		for i in range(0, len(args) ):
+			self.max.append( args[i] )
+
+		if len(self.min) != self.Dimensions:
+			raise SyntaxError("ERROR! Number of dimensions in Minboundary is not equal to number in circuit Initialisation")
+
 		self.maxboundarycheck = True
 
 
 	## Setting the minimum x y and z value of the force field
 	#
-	# @param *args will take in a coordiante  and if the cantilever goes outwith this boundary a warning is printed on screen.
+	# @param *args will take in a coordiante and if the cantilever goes outwith this boundary a warning is printed on screen.
 	#
 	# \b Example:
 	# \code{.py}
@@ -182,7 +151,7 @@ class Interpolate(Circuit):
 	# inter = machine.AddCircuit(type='Interpolate',name='inter', Filename = 'Force.dat', Dimensons = 3, Components = 3 ,pushed=True)
 	# inter.Minboundary(0,0,0)
 	# \endcode
-	#		
+	#                
 	# \code{.py}
 	# #For Dimensons = 2
 	# inter = machine.AddCircuit(type='Interpolate',name='inter', Filename = 'Force.dat', Dimensons = 2, Components = 2 ,pushed=True)
@@ -195,46 +164,54 @@ class Interpolate(Circuit):
 	# inter.Minboundary(0)
 	# \endcode
 	def Minboundary(self, *args):
-		self.minx = args[0]
-		if len(args) >= 2:
-			self.miny = args[1]
-		if len(args) == 3:
-			self.minz = args[2]
+			self.min = []
 
-		self.minboundarycheck = True
-
-
-		
-	def Update (self):
-		if self.maxboundarycheck == False:
-			raise SyntaxError("No max boundarys for the force field added!")
-
-		if self.minboundarycheck == False:
-			raise SyntaxError("No min boundarys for the force field added!")
-		if self.machine.time > 0:
-			if self.I["coord"].value[0] > self.maxx or self.I["coord"].value[0] < self.minx:
-				print "Warning tip is outside of the force field (x range) interpolation will output NaN" 
-			if self.I["coord"].value[1] > self.maxy or self.I["coord"].value[1] < self.miny:
-				print "Warning tip is outside of the force field (y range) interpolation will output NaN"
-			if self.I["coord"].value[2] > self.maxz or self.I["coord"].value[2] < self.minz:
-				print "Warning tip is outside of the force field (z range) interpolation will output NaN"
+			for i in range(0, len(args) ):
+					self.min.append( args[i] )
+			if len(self.min) != self.Dimensions:
+					raise SyntaxError("ERROR! Number of dimensions in Minboundary is not equal to number in circuit Initialisation")
+			self.minboundarycheck = True
 
 
-		if self.machine.time > 0:
-			self.O["Fx"].value =  numpy.interp(self.I["coord"].value[0], self.xvalues,  self.Fx)
-
-		if self.machine.time > 0 and self.Components > 1:
-			self.O["Fy"].value =  numpy.interp(self.I["coord"].value[1], self.xvalues,  self.Fy)
-		else: 
-			self.O["Fy"].value = 0
-
-		if self.machine.time > 0 and self.Components == 3:
-			self.O["Fz"].value =  numpy.interp(self.I["coord"].value[2], self.xvalues,  self.Fz)
-
-		if self.machine.time > 0 and self.Components == 1:
-			self.O["Fx"].value = self.i.__call__(self.I["coord"].value[0],self.I["coord"].value[1],self.I["coord"].value[2])
 			
-		else:
-			self.O["Fx"].value = 0			
-			self.O["Fy"].value = 0
-			self.O["Fz"].value = 0
+	def Update (self):
+			if self.maxboundarycheck == False:
+					raise SyntaxError("No max boundarys for the force field added!")
+
+			if self.minboundarycheck == False:
+					raise SyntaxError("No min boundarys for the force field added!")
+
+
+
+			if self.machine.time > 0 and self.errorcheck == False:
+
+
+					for i in range(0, self.Dimensions):
+
+							if type (self.I["coord"].value) == float:
+									raise SyntaxError("ERROR: The positon for the interpolation holder was not found, maybe you forgot to connect the circuit?")
+
+
+							if self.I["coord"].value[i] > self.max[i] or self.I["coord"].value[i] < self.min[i] :
+									print "warning outside of force field!"
+									self.errorcheck = True
+
+			if self.machine.time > 0 and self.errorcheck == True:
+					for i in range(0, self.Dimensions):
+							if self.I["coord"].value[i] <= self.max[i] and self.I["coord"].value[i] >= self.min[i] :
+									self.errorcheck = False
+
+
+			if self.machine.time > 0 and self.check == False:
+					for i in range(0 , self.Components):
+							self.O["F" + str(i+1) ].value = numpy.interp(self.I["coord"].value[i], self.pos[i], self.comp[i])
+
+
+
+			#special case of 3 dim and 1 comp
+			if self.machine.time > 0 and self.check == True:
+					for i in range(0 , self.Components):
+							self.O[ "F" + str(i+1) ].value = self.I.__call__(self.I["coord"].value[0],self.I["coord"].value[1],self.I["coord"].value[2])
+
+
+
--- src/vafmcircuits_Scanner.py
+++ src/vafmcircuits_Scanner.py
@@ -6,23 +6,24 @@ import vafmcircuits_Logic
 from vafmcircuits import Machine
 
 ## \package vafmcircuits_Scanner
-# This file contains the Scanning system 
-#
+# This file contains the Scanning system
+# \file vafmcircuits_Scanner.py
+# This file contains the Scanning system
 
 ## \brief Scanner circuit.
 #
 # This is the circuit that allows the user to control the cantilever placement.
 #
-# - \b Initialisation \b parameters:\n
-# 	- \a process = Name of the object which was used to initiate the machine.\n
+# - \b Initialisation \b parameters:
+# 	- \a process = Name of the object which was used to initiate the machine.
 #
-# - \b Input \b channels: \n
+# - \b Input \b channels: 
 # 	- This circuit has no input channels.
 #
-# - \b Output \b channels:\n
-#	- \a pos: The position of the cantilever.
-#	- \a ScanPos: The positon of the cantilever when using the Scan function
-#	- \a Record: This is either True or False and works in conjunction with the cantilever output signal, this is used to control when you want the cantilever to output a signal, by default this is set to True
+# - \b Output \b channels:
+#        - \a pos: The position of the cantilever.
+#        - \a ScanPos: The positon of the cantilever when using the Scan function
+#        - \a Record: This is either True or False and works in conjunction with the cantilever output signal, this is used to control when you want the cantilever to output a signal, by default this is set to True
 #
 # \b Example:
 # \code
@@ -32,25 +33,24 @@ from vafmcircuits import Machine
 #
 class scanner(Circuit):
 
-
 	
 	def __init__(self, machine, name, **keys):
-		
-		super(self.__class__, self).__init__( machine, name )
+			
+			super(self.__class__, self).__init__( machine, name )
 
-		if 'Process' in keys.keys():
-			self.machine = keys['Process']
-		else:
-			raise NameError("Missing process input!")
+			if 'Process' in keys.keys():
+					self.machine = keys['Process']
+			else:
+					raise NameError("Missing process input!")
 
-		self.AddOutput("pos")
-		self.AddOutput("ScanPos")
-		self.AddOutput("Record")
-		self.x = 0
-		self.y = 0
-		self.z = 0
+			self.AddOutput("pos")
+			self.AddOutput("ScanPos")
+			self.AddOutput("Record")
+			self.x = 0
+			self.y = 0
+			self.z = 0
 
-		self.O['Record'].value = True
+			self.O['Record'].value = True
 	
 	## Place the cantilever to a position instantly.
 	#
@@ -64,10 +64,10 @@ class scanner(Circuit):
 	#
 
 	def Place (self,x,y,z):
-		self.x = x
-		self.y = y
-		self.z = z
-		print "Cantilever places at position " +str(x) +str(y) +str(z)
+			self.x = x
+			self.y = y
+			self.z = z
+			print "Cantilever placed at position " +str(x) + " " +str(y)+ " " +str(z)
 
 	## Move the cantilever by a given translation vector at a given speed
 	#
@@ -82,30 +82,40 @@ class scanner(Circuit):
 	# \endcode
 	#
 	def Move (self,x,y,z,v):
-		self.deltax = x 
-		self.deltay = y 
-		self.deltaz = z 
+			self.deltax = x
+			self.deltay = y
+			self.deltaz = z
 
-		self.startingx = self.x
-		self.startingy = self.y
-		self.startingz = self.z
+			self.startingx = self.x
+			self.startingy = self.y
+			self.startingz = self.z
 
-		self.xsection = self.deltax * v*self.machine.dt
-		self.ysection = self.deltay * v*self.machine.dt
-		self.zsection = self.deltaz * v*self.machine.dt
-		time = int(self.deltax / self.xsection)
-		for i in range( time  ):
-			self.x = self.x + self.xsection
-			self.y = self.y + self.ysection
-			self.z = self.z + self.zsection
+			self.xsection = self.deltax * v*self.machine.dt
+			self.ysection = self.deltay * v*self.machine.dt
+			self.zsection = self.deltaz * v*self.machine.dt
 
-			if i == time-1:
-				self.x = self.startingx + self.deltax
-				self.y = self.startingy + self.deltay
-				self.z = self.startingz + self.deltaz
+			if self.deltax != 0:
+					time = int(self.deltax / self.xsection)
 
-			self.machine.Update()
-		print "Cantilever moved to position " +str( self.deltax ) + " " +str(self.deltax)+ " " +str(self.deltax) +" in " +str(time*self.machine.dt) + " seconds"		
+			if self.deltay != 0:
+					time = int(self.deltay / self.ysection)
+
+			if self.deltaz != 0:
+					time = int(self.deltaz / self.zsection)
+
+					
+			for i in range( time ):
+					self.x = self.x + self.xsection
+					self.y = self.y + self.ysection
+					self.z = self.z + self.zsection
+
+					if i == time:
+							self.x = self.startingx + self.deltax
+							self.y = self.startingy + self.deltay
+							self.z = self.startingz + self.deltaz
+
+					self.machine.Update()
+			print "Cantilever moved to position " +str( self.deltax ) + " " +str(self.deltay)+ " " +str(self.deltaz) +" in " +str(time*self.machine.dt) + " seconds"                
 
 
 
@@ -123,30 +133,38 @@ class scanner(Circuit):
 	#
 	def MoveTo (self,x,y,z,v):
 
-		self.deltax = x - self.x 
-		self.deltay = y - self.y
-		self.deltaz = z - self.z
+			self.deltax = x - self.x
+			self.deltay = y - self.y
+			self.deltaz = z - self.z
 
-		self.startingx = self.x
-		self.startingy = self.y
-		self.startingz = self.z
+			self.startingx = self.x
+			self.startingy = self.y
+			self.startingz = self.z
 
-		self.xsection = self.deltax * v*self.machine.dt
-		self.ysection = self.deltay * v*self.machine.dt
-		self.zsection = self.deltaz * v*self.machine.dt
-		time = int(self.deltax / self.xsection)
+			self.xsection = self.deltax * v*self.machine.dt
+			self.ysection = self.deltay * v*self.machine.dt
+			self.zsection = self.deltaz * v*self.machine.dt
+			
+			if self.deltax != 0:
+					time = int(self.deltax / self.xsection)
 
-		for i in range( time +1):
-			self.x = self.x + self.xsection
-			self.y = self.y + self.ysection
-			self.z = self.z + self.zsection
-			self.machine.Update()
+			if self.deltay != 0:
+					time = int(self.deltay / self.ysection)
 
-		if i == time-1:
-				self.x = self.startingx + self.deltax
-				self.y = self.startingy + self.deltay
-				self.z = self.startingz + self.deltaz
-		print "Cantilever moved to position " +str(x) +str(y) +str(z) +" in " +str(time*self.machine.dt) + " seconds"	
+			if self.deltaz != 0:
+					time = int(self.deltaz / self.zsection)
+
+			for i in range( time +1):
+					self.x = self.x + self.xsection
+					self.y = self.y + self.ysection
+					self.z = self.z + self.zsection
+					self.machine.Update()
+
+			if i == time:
+							self.x = x
+							self.y = y
+							self.z = z
+			print "Cantilever moved to position " +str(x)+ " " +str(y)+ " " +str(z) +" in " +str(time*self.machine.dt) + " seconds"        
 
 	## Causes the cantilever to remain at this current position for a given time
 	#
@@ -160,15 +178,15 @@ class scanner(Circuit):
 	#
 	def Wait (self, time):
 
-		for i in range(int (time/self.machine.dt) +1):
-			self.machine.Update()
-		print "waited " + str(time) + " seconds"
+			for i in range(int (time/self.machine.dt) +1):
+					self.machine.Update()
+			print "waited " + str(time) + " seconds"
 
 
 	## Scans the cantilever along a given distane, in a given direction with a given speed and records points at a given interval.
 	#
-	# @param x y and z is the direction the cnatilever will move in. \n
-	#length is the distance the cantilever will travel. \n
+	# @param x y and z is the direction the cnatilever will move in. 
+	#length is the distance the cantilever will travel. 
 	#v is the speed, v=1 means the scan will be complete in 1 second, for example if the speed is increased to 2 then the scan will be completed in 0.5 seconds.
 	#pts is how many iterations of the loop will be compleed until an output is produced. This is determined by the step size so for example if you wanted a output every 0.5 seconds with a step size
 	#of 0.1 then you must set pts = 5 as 5 iterations of the loop will be completed before reaching 0.5 seconds.
@@ -180,56 +198,64 @@ class scanner(Circuit):
 	# \endcode
 	#
 	def Scan (self,x,y,z,length,v,pts):
-		self.O['Record'].value = False
-		
-		self.deltax = x * length
-		self.deltay = y * length
-		self.deltaz = z * length
+			self.O['Record'].value = False
+			
+			self.deltax = x * length
+			self.deltay = y * length
+			self.deltaz = z * length
 
-		counter = 0
-		test = 0
-		
-		self.startingx = self.x
-		self.startingy = self.y
-		self.startingz = self.z
+			counter = 0
+			test = 0
+			
+			self.startingx = self.x
+			self.startingy = self.y
+			self.startingz = self.z
 
-		self.xsection = self.deltax * v*self.machine.dt
-		self.ysection = self.deltay * v*self.machine.dt
-		self.zsection = self.deltaz * v*self.machine.dt
+			self.xsection = self.deltax * v*self.machine.dt
+			self.ysection = self.deltay * v*self.machine.dt
+			self.zsection = self.deltaz * v*self.machine.dt
 
 
 
-		time = int(self.deltax/self.xsection)
+			if self.deltax != 0:
+					time = int(self.deltax / self.xsection)
 
-		for i in range( time +1):
+			if self.deltay != 0:
+					time = int(self.deltay / self.ysection)
 
-			counter = counter + 1
-			self.x = self.x + self.xsection
-			self.y = self.y + self.ysection
-			self.z = self.z + self.zsection
+			if self.deltaz != 0:
+					time = int(self.deltaz / self.zsection)
 
-			#print self.x , self.y , self.z
-			if i == time-1:
-				self.x = self.startingx + length *x
-				self.y = self.startingy + length *y
-				self.z = self.startingz + length *z
-			if pts == counter:
-				self.O['Record'].value = True
-				counter = 0
-				
-			self.machine.Update()	
-		print "Cantilever moved to position " +str(x) +str(y) +str(z) +" in " +str(time*self.machine.dt) + " seconds"	
+			for i in range( time +1):
+
+					counter = counter + 1
+					self.x = self.x + self.xsection
+					self.y = self.y + self.ysection
+					self.z = self.z + self.zsection
+
+					#print self.x , self.y , self.z
+					if i == time:
+							self.x = self.startingx + length *x
+							self.y = self.startingy + length *y
+							self.z = self.startingz + length *z
+					if pts == counter:
+							self.O['Record'].value = True
+							counter = 0
+							
+					self.machine.Update()        
+			print "Cantilever moved to position " +str(x) +str(y) +str(z) +" in " +str(time*self.machine.dt) + " seconds"        
 
 	def Loop (self, func, time):
 
-		pass
+			pass
 
 
 	def Initialize (self):
-		
-		pass
+			
+			pass
 
-		
+			
 	def Update (self):
 
-		self.O['pos'].value = [self.x, self.y, self.z]
+			self.O['pos'].value = [self.x, self.y, self.z]
+
