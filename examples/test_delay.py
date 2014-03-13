a0a7aad16fc5a2181caaf834ff2186d9062b7ef2
fullmetalfelix <vampirer@tiscalinet.it>
2014-03-13 11:47:02 +0900

    fix test units

--- examples/test_delay.py
+++ examples/test_delay.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
-from vafmbase import ChannelType
 from vafmcircuits import Machine
-
-import vafmcircuits
-import vafmcircuits_signal_processing
 
 
 def main():
@@ -12,16 +8,13 @@ def main():
 	
 	machine = Machine(name='machine', dt=0.01, pushed=True);
 	
-	
 	#Add Circuits
-	
-	
   	machine.AddCircuit(type='waver',name='osc', amp=1, freq=1, pushed=True )
 	machine.AddCircuit(type='delay', name='lag', DelayTime=0.2, pushed=True)
 	
 	machine.Connect("osc.sin","lag.signal")
 
-	out1 = machine.AddCircuit(type='output',name='output',file='test_delay.log', dump=1)
+	out1 = machine.AddCircuit(type='output',name='output',file='test_delay.out', dump=1)
 	out1.Register('global.time', 'osc.sin', 'lag.out')
 	
 	machine.Wait(5)
