129c8f4eb7c6f37de93489499214de62b62616cf
fullmetalfelix <vampirer@tiscalinet.it>
2014-04-03 11:00:59 +0900

    premerge2

---
+++ examples/test_VDW.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from vafmbase import ChannelType
+from vafmcircuits import Machine
+
+
+def main():
+
+
+	machine = Machine(name='machine', dt=0.01, pushed=True);
+
+	#Add Circuits
+	scanner = machine.AddCircuit(type='Scanner',name='scan', pushed=True )
+	machine.AddCircuit(type='VDW', name='VDW', alpha=0.28658, hamaker=39.6e-20, radius=3.9487, offset=0, pushed=True)
+
+
+	#debug output
+	out1 = machine.AddCircuit(type='output',name='output',file='VDW.out', dump=1)
+	out1.Register("scan.z", "VDW.fz")
+
+
+	machine.Connect("scan.z","VDW.ztip")
+
+	scanner.Place(x=0,y=0,z=10)
+	scanner.Move(x=0,y=0,z=10.1,v=1)
+
+if __name__ == '__main__':
+	main()
+
